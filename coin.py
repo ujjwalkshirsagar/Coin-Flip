65acce7b8ed4754b20fd3e45f5d6f9b148667b83
ujjwalkshirsagar <84407884+ujjwalkshirsagar@users.noreply.github.com>
2021-05-19 00:14:53 +0530

    Create coin.py
    Write some code that simulates flipping a single coin however many times the user decides. The code should record the outcomes and count the number of tails and heads.

---
+++ coin.py
@@ -0,0 +1,15 @@
+import random
+
+def coin_flip(times):
+    heads=0
+    tails=0
+    for x in range(times):
+        i=random.choice([0,1])
+        if i==0:
+            heads+=1
+            print ('heads')
+        else:
+            tails+=1
+            print('tails')
+    print ('heads: ' + str(heads))
+    print ('tails: ' + str(tails))
